73903366ff0e317c99728b76533fc4ad8037c615
thenordplace <sadapan@gmail.com>
2023-01-26 17:18:53 -0700

    got adding roles to work

--- inquirer.js
+++ inquirer.js
@@ -1,20 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const cTable = require ('console.table');
-const mysql = require ('mysql2');
+const cTable = require('console.table');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // TODO: Add MySQL password here
-      password: process.env.DB_PASSWORD,
-      database: 'company_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: process.env.DB_PASSWORD,
+        database: 'company_db'
     },
     console.log(`Connected to the company_db database.`)
-  );
+);
 
 const options = [
     {
@@ -24,123 +24,130 @@ const options = [
         choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
     },
 ]
-function viewDepartments () {
+function viewDepartments() {
     console.log("testing")
-    db.query("select * from department", (err, rows)=> {
+    db.query("select * from department", (err, rows) => {
         console.table(rows);
         showOptions()
     })
 }
-function viewRole () {
-    db.query("select * from role", (err, rows)=> {
+function viewRole() {
+    db.query("select * from role", (err, rows) => {
         console.table(rows);
         showOptions()
     })
 }
-function viewEmployee () {
-    db.query("select * from employee", (err, rows)=> {
+function viewEmployee() {
+    db.query("select * from employee", (err, rows) => {
         console.table(rows);
         showOptions()
     })
 }
 
 
-function addDepartment () {
+function addDepartment() {
     inquirer.prompt({
         type: "input",
         message: "What is the name of the department?",
         name: "departmentName",
-    }). then ((answers) => {
-        db.query ("INSERT INTO department(name)values(?)", [answers.departmentName], (err, rows) => {
+    }).then((answers) => {
+        db.query("INSERT INTO department(name)values(?)", [answers.departmentName], (err, rows) => {
             console.table(rows);
             showOptions()
         })
-    }) 
+    })
 }
-function addRole () {
+function addRole() {
     inquirer.prompt([{
         type: "input",
         message: "What is the name of the role?",
         name: "roleTitle",
     },
     {
-        type:"list",
+        type: "list",
         message: "What is the role's salary?",
         name: "roleSalary",
         choices: [10, 5, 2, 1]
     },
     {
-        type:"list",
+        type: "list",
         message: "What department does this role belong to?",
         name: "roleDepartment",
         choices: ["Sales", "Marketing", "Good ol Fun"]
-    }]). then ((answers) => {
+    }]).then((answers) => {
         console.log(answers.roleTitle, answers.roleSalary, answers.roleDepartment)
-        db.query ("INSERT INTO role(answers)values(?)", [answers.roleTitle, answers.roleSalary, answers.roleDepartment], (err, rows) => {
-            console.table(rows);})
+        db.query("INSERT INTO role set ?", {title:answers.roleTitle, salary: answers.roleSalary, department_id:1}, (err, rows) => {
+            console.table(rows);
+        })
         showOptions()
-    }) 
+    })
 }
-function addEmployee () {
+function addEmployee() {
     inquirer.prompt([{
         type: "input",
         message: "What is the first name of the new employee?",
         name: "employeeFirstName",
-    }, 
+    },
     {
         type: "input",
         message: "What is the employee's last name?",
         name: "employeeLastName",
-    }, 
+    },
     {
         type: "list",
         message: "What is the employee's role?",
         name: "employeeRole",
-        choices: ["Manager", "Supervisor","Jerk","Peon"],
+        choices: ["Manager", "Supervisor", "Jerk", "Peon"],
     },]
-    ). then ((answers) => {
-        db.query ("INSERT INTO employee(first_name, last_name, role_id)values(?)", [answers.employeeFirstName, answers.employeeLastName, answers.employeeRole], (err, rows) => {
+    ).then((answers) => {
+        db.query("INSERT INTO employee(first_name, last_name, role_id)values(?)", [answers.employeeFirstName, answers.employeeLastName, answers.employeeRole], (err, rows) => {
             console.table(rows);
             showOptions()
         })
-    }) 
+    })
 }
-function updateEmployee () {
-    inquirer.prompt({
+function updateEmployee() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the employee's id?",
+        name: "employeeID",
+    },
+    {
         type: "list",
         message: "What is the employee's new role?",
         name: "employeeRole",
         choices: ["Manager", "Supervisor", "Jerk", "Peon"]
 
-    }). then ((answers) => {
-        db.query ("UPDATE employee SET role_id = ? WHERE id = ?", [answers.employeeRole], (err, rows) => {
+    }]).then((answers) => {
+        db.query("UPDATE employee SET role_id = ? WHERE id = ?", [answers.employeeRole], (err, rows) => {
             console.table(rows);
             showOptions()
         })
-    }) 
+    })
 }
 
 
 
 function showOptions() {
     inquirer.prompt(options).then((answers => {
-    if (answers.action === "View all departments") { //tried changing options.actions to answers
-        viewDepartments()
-        console.log("Yippee");
-    } else if (answers.action === "View all roles") {
-        viewRole();
-    } else if (answers.action === "View all employees") {
-        viewEmployee();
-    } else if (answers.action === "Add a department") {
-       addDepartment();
-    } else if (answers.action === "Add a role") {
-        addRole();
-    } else if (answers.action === "Add an employee") {
-        addEmployee();
-    } else if (answers.action === "Update an employee role") {
-        updateEmployee();
-    }
-}))}
+        if (answers.action === "View all departments") { //tried changing options.actions to answers
+            viewDepartments()
+            console.log("Yippee");
+        } else if (answers.action === "View all roles") {
+            viewRole();
+        } else if (answers.action === "View all employees") {
+            viewEmployee();
+        } else if (answers.action === "Add a department") {
+            addDepartment();
+        } else if (answers.action === "Add a role") {
+            addRole();
+        } else if (answers.action === "Add an employee") {
+            addEmployee();
+        } else if (answers.action === "Update an employee role") {
+            updateEmployee();
+        }
+    }))
+}
 
 showOptions()
 
